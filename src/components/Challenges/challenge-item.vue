d4a28321ec6199631a717c3166228f4223803e46
Dimanda Traper <traperwaze@gmail.com>
2022-08-17 20:53:29 +0700

    add challenge list and item components

---
+++ src/components/Challenges/challenge-item.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: ['challenge'],
+  methods: {
+    chalOnClick(chalcode) {
+      console.log(chalcode);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="challenge-item" @click="chalOnClick(challenge.code)">
+    <b class="title">{{ challenge.name }}</b>
+    <p class="description" title="Description">
+      {{ challenge.description || 'empty' }}
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+@import './style.scss';
+</style>
---
+++ src/components/Challenges/challenge-list.vue
@@ -0,0 +1,14 @@
+<script setup>
+import chals from "../../data/challenges"
+import ChallengeItem from "./challenge-item.vue";
+</script>
+
+<template>
+  <div class="challenge-list">
+    <ChallengeItem v-for="chal in chals" :key="chal.code" :challenge="chal"/>
+  </div>
+</template>
+
+<style lang="scss">
+@import './style.scss';
+</style>
