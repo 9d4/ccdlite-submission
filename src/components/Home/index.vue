6de586827e7a535c6205588c22c33275d51c9af9
Dimanda Traper <traperwaze@gmail.com>
2022-08-17 20:53:43 +0700

    show list at home page

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,10 +1,13 @@
 <script setup>
+import ChallengeList from "../Challenges/challenge-list.vue"
 </script>
 
 <template>
   <div class="homepage">
     <h1 class="title">Welcome to CCDLITE Submission</h1>
     <p>This place is where you can submit your flags.</p>
+
+    <ChallengeList />
   </div>
 </template>
 
