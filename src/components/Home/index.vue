639de79bc29e4ceaf08ceca4f8fc25a2b77859fd
Dimanda Traper <traperwaze@gmail.com>
2022-08-17 20:21:09 +0700

    add homepage component

---
+++ src/components/Home/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div class="homepage">
+    <h1 class="title">Welcome to CCDLITE Submission</h1>
+    <p>This place is where you can submit your flags.</p>
+  </div>
+</template>
+
+<style lang="scss">
+@import './style.scss';
+</style>
