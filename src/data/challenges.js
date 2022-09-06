d0479dfd0d125eba413ada5db7035a5bb5bcd56a
Dimas Ananda traper <traperwaze@gmail.com>
2022-09-06 07:05:50 +0700

    add chapter2 submission

--- src/data/challenges.js
+++ src/data/challenges.js
@@ -4,6 +4,11 @@ const challenges = [
     description: '',
     code: 'ccd-ch-1',
   },
+  {
+    name: 'Chapter 2',
+    description: 'no clue',
+    code: 'ccd-ch-2',
+  }
 ];
 
 export default challenges;
